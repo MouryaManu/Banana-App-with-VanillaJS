6fc9ceb816d41b913200c6f71e1feae4413a99bb
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-04-20 10:45:13 +0530

    created new css doc to link

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var click=document.querySelector('#click');
-function takeName()
-{
-   
-    console.log("clicked")
+var input=document.querySelector("#txt-input");
+var output=document.querySelector("#txt-output");
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function translate(text) {
+    return serverURL + "?" + "text=" + text
 }
-click.addEventListener("click",takeName);
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something wrong with server. Please try again after some time.")
+}
+
+function clickfunction()
+{
+    var inputtxt=input.value;
+    fetch(translate(inputtxt))
+   .then(response => response.json())
+   .then(json => {
+       var translatedtext= json.contents.translated;
+       output.innerText= translatedtext;
+   })
+};
+click.addEventListener("click",clickfunction);
